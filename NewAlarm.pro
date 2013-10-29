c3c986f26100d41a2cba7393b1b5cdf16c8748e1
huellif <huellif@gmail.com>
2013-10-29 14:25:59 +0100

    v1.1

--- NewAlarm.pro
+++ NewAlarm.pro
@@ -1,5 +1,13 @@
 symbian:{
 TARGET.UID3 = 0x20071ae2
+LIBS += -lapparc -lapgrfx
+VERSION = 1.1.0
+
+    my_deployment.pkg_prerules += vendorinfo
+
+    DEPLOYMENT += my_deployment
+
+    vendorinfo += "%{\"huellifSoft\"}" ":\"huellifSoft\""
 }
 
 SOURCES += main.cpp
--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
-#include <QProcess>
+#include <apacmdln.h>
+#include <APGCLI.H>
 
 int main(int argc, char *argv[])
 {
-    QProcess *myProcess = new QProcess;
-    myProcess->start("alarmeditorapp.exe");
+    CApaCommandLine* commandLine = CApaCommandLine::NewLC();
+    commandLine->SetCommandL(EApaCommandRun);
+    commandLine->SetExecutableNameL(_L("alarmeditorapp.exe"));
+    RApaLsSession apaLsSession;
+    User::LeaveIfError(apaLsSession.Connect());
+    CleanupClosePushL(apaLsSession);
+    User::LeaveIfError(apaLsSession.StartApp(*commandLine));
+    CleanupStack::PopAndDestroy(&apaLsSession);
+    CleanupStack::PopAndDestroy(commandLine);
     return 1;
 }
